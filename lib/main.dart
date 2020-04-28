a9e20ca3c741348f92f8b846ec4b218221f91939
Eduardo Rosas <lalorosas@outlook.com>
2020-04-28 15:17:28 -0500

    Work w/ circle images, icons and Cards

--- lib/main.dart
+++ lib/main.dart
@@ -9,36 +9,78 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.indigo,
         body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              width: 100.0,
-              color: Colors.red,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.yellow,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Me.png'),
+              ),
+              Text(
+                'Eduardo Rosas',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'MOBILE DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.amberAccent,
+                  letterSpacing: 3.5,
+                  fontWeight: FontWeight.bold,
                 ),
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.green,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
                 ),
-              ],
-            ),
-            Container(
-              width: 100.0,
-              color: Colors.blue,
-            ),
-          ],
-        )),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 25.0,
+                    color: Colors.indigo,
+                  ),
+                  title: Text(
+                    '+52 123 456 7890',
+                    style: TextStyle(
+                      color: Colors.indigo,
+                      fontSize: 16.0,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 25.0,
+                    color: Colors.indigo,
+                  ),
+                  title: Text(
+                    'lalo@eduardorosas.mx',
+                    style: TextStyle(
+                      color: Colors.indigo,
+                      fontSize: 16.0,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
